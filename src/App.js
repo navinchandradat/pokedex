6c8ff9cb840b18efb1704aa351c2d16bcbd6818f
navinchandradat <navin.chandradat.dev@gmail.com>
2020-06-17 00:10:39 -0400

    Introduced Local State to App.js
    Adding Local State to the Pokemon Header component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,24 @@ import React from 'react';
 import PokedexHeader from './components/PokedexHeader'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <PokedexHeader author={' @navin_c'} name={'Navin'} />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state= {
+      name: "Navin"
+    }
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <PokedexHeader 
+         author={'@navin_c'} 
+         name={this.state.name} 
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
