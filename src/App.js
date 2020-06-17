c2c4d563ae52aac464f92902863385751574f36c
navinchandradat <navin.chandradat.dev@gmail.com>
2020-06-16 23:57:51 -0400

    Update App.js
    Changed spacing on author line

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <PokedexHeader author={'@navin_c'} name={'Navin'} />
+      <PokedexHeader author={' @navin_c'} name={'Navin'} />
     </div>
   );
 }
